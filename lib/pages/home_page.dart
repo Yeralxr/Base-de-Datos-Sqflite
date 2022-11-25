dadd1c3f34cbb9e5af7bcdfee66d7dd5480f0f20
juan <201713632a@utea.edu.pe>
2022-11-25 01:24:18 -0500

    progreso (60%)

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,28 @@
+import 'package:base_de_datos_sqflite/db/db_admin.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                DBAdmin.db.initDatabase();
+              },
+              child: Text("Mostrar Data"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
